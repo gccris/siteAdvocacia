a09eaea444efe9a198ddb39447a37984c62cbd06
Gabriel Campos Cristiano <gabrielccristiano@gmail.com>
2017-08-16 22:58:16 -0300

    Contato/maps

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,11 @@
+var request = new XMLHttpRequest();
+this.request.open(this.method, this.url, true);
+request.setRequestHeader( 'Access-Control-Allow-Origin', '*');
+request.setRequestHeader( 'Content-Type', 'application/json' );
+
+
 var slideIndex = 1;
 showDivs(slideIndex);
-
 function plusDivs(n) {
 	showDivs(slideIndex += n);
 }
@@ -13,22 +18,23 @@ function showDivs(n) {
 	var i;
 	var slide = document.getElementsByClassName("slide");
 	var dots = document.getElementsByClassName("slider-page");
-
-	if (n > slide.length) {slideIndex = 1}    
-	if (n < 1) {slideIndex = slide.length}
-	for (i = 0; i < slide.length; i++) {
-		slide[i].style.display = "none";  
+	if (slide.length != 0){
+		if (n > slide.length) {slideIndex = 1}    
+		if (n < 1) {slideIndex = slide.length}
+		for (i = 0; i < slide.length; i++) {
+			slide[i].style.display = "none";  
+		}
+		for (i = 0; i < dots.length; i++) {
+			dots[i].className = dots[i].className.replace(" active", "");
+		}
+		slide[slideIndex-1].style.display = "block";  
+		dots[slideIndex-1].className += " active";
+		slideIndex++;
+		if (slideIndex > slide.length) {
+			slideIndex = 1
+		} 
+		setTimeout(showDivs, 5000); // Change image every 5 seconds
 	}
-	for (i = 0; i < dots.length; i++) {
-		dots[i].className = dots[i].className.replace(" active", "");
-	}
-	slide[slideIndex-1].style.display = "block";  
-	dots[slideIndex-1].className += " active";
-	slideIndex++;
-	if (slideIndex > slide.length) {
-		slideIndex = 1
-	} 
-	setTimeout(showDivs, 5000); // Change image every 5 seconds
 }
 
 function openPage(n){
@@ -39,4 +45,8 @@ function openPage(n){
 		x[i].className = x[i].className.replace(" active", "");
 	}
 	x[n-1].className += " active";
+	if(n == 5) // contato
+		$('#container').load('http://localhost/site/contato.html');
+	if(n == 6) // maps
+		$('#container').load('http://localhost/site/maps.html');
 }
