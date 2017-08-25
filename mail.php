e35b15f2387704eb8384d376a65eefcfd2885db3
Gabriel Campos Cristiano <gabrielccristiano@gmail.com>
2017-08-25 12:49:52 -0300

    Contato por email
    Contato por email feito. Talvez nao seja enviado o email por erros no xaamp
    Falta redirecionar a pagina corretamente

---
+++ mail.php
@@ -0,0 +1,11 @@
+<?php
+$to = "gabrielccristiano@gmail.com";
+$subject = "Contato feito por ". $_POST["name"];
+$txt = $_POST["message"]."\n
+ Telefone de contato:". $_POST["telefone"];
+
+$headers = "From: ". $_POST["email"];
+echo "<script>alert('Email enviado com Sucesso!);location.href=\"index.html\";</script>";
+mail($to,$subject,$txt,$headers);
+
+?>
