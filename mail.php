14c40b4eb0fdff8a71f8c7cbdc7ab5e7e33eb258
Gabriel Campos Cristiano <gabrielccristiano@gmail.com>
2018-02-22 20:11:02 -0300

    bug fix
    bug fix texto e mail

--- mail.php
+++ mail.php
@@ -1,11 +1,11 @@
 <?php
-$to = "giriboniadvocacia@gmail.com";
+$to = "roger@giriboni.adv.br";
 $subject = "Contato feito por ". $_POST["name"];
 $txt = $_POST["message"]."\n
  Telefone de contato:". $_POST["telefone"];
 
 $headers = "From: ". $_POST["email"];
-echo "<script>alert('Email enviado com Sucesso!);location.href=\"index.html\";</script>";
 mail($to,$subject,$txt,$headers);
-
+echo "<script>alert('Contato feito com sucesso. Seu email será respondido o mais rápido possivel');</script>";
+include("index.html");
 ?>
